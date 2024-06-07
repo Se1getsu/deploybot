85a719c1bef6df8b4b3995a13d2bda4a4a882a58
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-06-07 11:55:24 +0900

    feat: Implement options: `-v`, `-h`

--- dpb.pl
+++ dpb.pl
@@ -6,20 +6,44 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use Service::Factory;
+use Getopt::Long;
+
+my $_version = "1.0.0-dev";
+
+sub show_version {
+    print "dpb version $_version\n";
+}
+
+sub show_usage {
+    print <<EOD;
+usage: $0 [-v | --version] [-h | --help] <command> [<args>]
+
+Options:
+  -h    print this help message and exit
+  -v    print the dpb version number and exit
+
+Commands:
+  hello   send "Hello, world!" as an dpb info log
+  run     start target execution and periodic deployments
+  log     print the list of webhooks configured to receive logs
+  set-log edit the list of webhooks configured to receive logs
+EOD
+}
+
+GetOptions(
+    "version|v"  => sub { show_version; exit; },
+    "help|h"  => sub { show_usage; exit; },
+) or exit 1;
 
 my %subcommands = (
-    'hello'   => 'Dpb::Hello',  # TODO: Delete this sample subcommand
+    'hello'   => 'Dpb::Hello',
     'run'     => 'Dpb::Run',
     'log'     => 'Dpb::Log',
     'set-log' => 'Dpb::SetLog',
 );
 
 my $subcommand = shift @ARGV;
-
-unless ($subcommand) {
-    print "Usage: $0 <subcommand> [options]\n";
-    exit 1;
-}
+unless ($subcommand) { show_usage; exit 1; }
 
 if (exists $subcommands{$subcommand}) {
     my $module = $subcommands{$subcommand};
